0e28ab8d6634d4c477b4a54b7a83c46b99e8009d
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-05-10 21:54:11 -0300

    Finalizado o metodo inserir na UsuarioDAL
    Amanhã criaremos o banco de Dados e testaremos o insert na tabla usuário

--- Base/DAL/ComandoSQL.cs
+++ Base/DAL/ComandoSQL.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL
 {
-    class ComandoSQL
+    public  class ComandoSQL
     {
         private SqlCommand comando;
         private List<ParametroSQL> parametros;
