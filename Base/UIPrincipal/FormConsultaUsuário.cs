6fbc0e5135fff42e46c14ff0080de25267b07ac1
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-05-16 22:01:41 -0300

    inicio do formulário
    Foi iniciado o formulário principal mas o código  está com bug

---
+++ Base/UIPrincipal/FormConsultaUsuário.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIPrincipal
+{
+    public partial class FormConsultaUsuário : Form
+    {
+        public FormConsultaUsuário()
+        {
+            InitializeComponent();
+        }
+
+        private void FormConsultaUsuário_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSair_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
